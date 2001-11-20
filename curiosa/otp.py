2fb4c2dd8781813f6b6f1a1dca267c54881a2985
akuchling <akuchling@rivest.dlitz.net>
2001-11-20 16:04:50 -0700

    [project @ akuchling-20011120230450-9084e111c285c786] [project @ 2001-11-20 15:04:50 by akuchling] Remove these little curiosities

--- curiosa/otp.py
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/python 
-from sys import*;t=p=1;s=stdout;[i,j]=map(lambda f: open(f, 'r'), argv[1:3])
-while(t and p):t,p=i.read(1),j.read(1);t and p and s.write(chr(ord(t)^ord(p)))
--- curiosa/rsa.py
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/python 
-from sys import*;from string import*;a=argv;[s,p,q]=filter(lambda x:x[:1]!=
-'-',a);d='-d'in a;e,n=atol(p,16),atol(q,16);l=(len(q)+1)/2;o,inb=l-d,l-1+d
-while s:s=stdin.read(inb);s and map(stdout.write,map(lambda i,b=pow(reduce(
-lambda x,y:(x<<8L)+y,map(ord,s)),e,n):chr(b>>8*i&255),range(o-1,-1,-1)))
