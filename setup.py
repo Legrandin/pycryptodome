84d67591199284e7d23296f12b0feef2cd2dd885
akuchling <akuchling@rivest.dlitz.net>
2002-04-03 18:45:09 -0700

    [project @ akuchling-20020404014509-3215ed39d4587db5] [project @ 2002-04-03 17:45:09 by akuchling] Add setup.py builder

---
+++ setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+from distutils.core import setup, Extension
+
+setup(name="amkCrypto",
+      version="1.9a1",
+      description="Cryptographic modules for Python.",
+      author="A.M. Kuchling",
+      author_email="akuchlin@mems-exchange.org",
+      url="http://pycrypto.sourceforge.net",
+      
+      packages = ["Crypto", "Crypto.Hash", "Crypto.Cipher", "Crypto.Util",
+                  "Crypto.PublicKey"],
+      package_dir = { "Crypto":"." },
+      ext_modules = [
+                     # Hash functions
+                     Extension("Crypto.Hash.MD2",
+                               include_dirs=['src/'],
+                               sources=["hash/MD2.c"]),
+                     Extension("Crypto.Hash.MD4",
+                               include_dirs=['src/'],
+                               sources=["hash/MD4.c"]),
+                     Extension("Crypto.Hash.RIPEMD",
+                               include_dirs=['src/'],
+                               sources=["hash/RIPEMD.c"]),
+                     
+                     #Extension("Crypto.Cipher.DES",
+                     #include_dirs=['src/'],
+                     #sources=["block/DES.c"])
+                    ]
+     )
+
+      
+
