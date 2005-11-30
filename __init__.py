fbb0a1f9dee47a1f0d801b3e8bdbb4e7a5495675
akuchling <akuchling@rivest.dlitz.net>
2005-11-29 17:24:52 -0700

    [project @ akuchling-20051130002452-972149cb8c255ffe] [project @ 2005-11-29 16:24:52 by akuchling] Bump version

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ Crypto.Util               Various useful modules and functions (long-to-string
 
 __all__ = ['Cipher', 'Hash', 'Protocol', 'PublicKey', 'Util']
 
-__version__ = '2.0.1'
-__revision__ = "$Id: __init__.py,v 1.12 2005-06-14 01:20:22 akuchling Exp $"
+__version__ = '2.0.2'
+__revision__ = "$Id: __init__.py,v 1.13 2005-11-29 16:24:52 akuchling Exp $"
 
 
