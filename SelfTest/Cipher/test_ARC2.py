45540f34ca2051babdc26f1ce8d96f6684102c98
Dwayne C. Litzenberger <dlitz@dlitz.net>
2008-09-13 11:32:11 -0400

    SelfTest: Add tests for ARC2 (which we currently fail)

--- SelfTest/Cipher/test_ARC2.py
+++ SelfTest/Cipher/test_ARC2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-#  SelfTest/Cipher/__init__.py: Self-test for cipher modules
+#  SelfTest/Cipher/ARC2.py: Self-test for the Alleged-RC2 cipher
 #
 # =======================================================================
 # Copyright (C) 2008  Dwayne C. Litzenberger <dlitz@dlitz.net>
@@ -26,22 +26,40 @@
 # =======================================================================
 #
 
-"""Self-test for cipher modules"""
+"""Self-test suite for Crypto.Cipher.ARC2"""
 
 __revision__ = "$Id$"
 
-import unittest
+# This is a list of (plaintext, ciphertext, key[, description]) tuples.
+test_data = [
+    # Test vectors from RFC 2268
+
+    # 63-bit effective key length
+#    ('0000000000000000', 'ebb773f993278eff', '0000000000000000', "RFC2268-1"),
+
+    # 64-bit effective key length
+    ('ffffffffffffffff', '278b27e42e2f0d49', 'ffffffffffffffff', "RFC2268-2"),
+    ('1000000000000001', '30649edf9be7d2c2', '3000000000000000', "RFC2268-3"),
+    ('0000000000000000', '61a8a244adacccf0', '88',               "RFC2268-4"),
+    ('0000000000000000', '6ccf4308974c267f', '88bca90e90875a',   "RFC2268-5"),
+    ('0000000000000000', '1a807d272bbe5db1',
+        '88bca90e90875a7f0f79c384627bafb2', "RFC2268-6"),
+
+    # 128-bit effective key length
+    ('0000000000000000', '2269552ab0f85ca6',
+        '88bca90e90875a7f0f79c384627bafb2', "RFC2268-7"),
+    ('0000000000000000', '5b78d3a43dfff1f1',
+        '88bca90e90875a7f0f79c384627bafb216f80a6f85920584c42fceb0be255daf1e',
+        "RFC2268-8"),
+]
 
 def make_testsuite():
-    ts = unittest.TestSuite()
-    import test_ARC2;     ts.addTest(test_ARC2.make_testsuite())
-    import test_Blowfish; ts.addTest(test_Blowfish.make_testsuite())
-    import test_CAST;     ts.addTest(test_CAST.make_testsuite())
-    import test_IDEA;     ts.addTest(test_IDEA.make_testsuite())
-    import test_RC5;      ts.addTest(test_RC5.make_testsuite())
-    return ts
+    from Crypto.Cipher import ARC2
+    from common import make_block_testsuite
+    return make_block_testsuite(ARC2, "ARC2", test_data)
 
 if __name__ == '__main__':
+    import unittest
     unittest.main(defaultTest='make_testsuite')
 
 # vim:set ts=4 sw=4 sts=4 expandtab:
