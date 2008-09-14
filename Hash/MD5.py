d2311689910240e425741a546576129f4c9735e2
Dwayne C. Litzenberger <dlitz@dlitz.net>
2008-09-14 15:30:59 -0400

    Python 2.6 compatibility: When possible, use hashlib instead of the deprecated 'md5' and 'sha' modules

--- Hash/MD5.py
+++ Hash/MD5.py
@@ -3,11 +3,21 @@
 
 __revision__ = "$Id$"
 
-from md5 import *
+__all__ = ['new', 'digest_size']
 
-import md5
-if hasattr(md5, 'digestsize'):
-    digest_size = digestsize
-    del digestsize
-del md5
+try:
+    # The md5 module is deprecated in Python 2.6, so use hashlib when possible.
+    import hashlib
+    def new(data=""):
+        return hashlib.md5(data)
+    digest_size = new().digest_size
+
+except ImportError:
+    from md5 import *
+
+    import md5
+    if hasattr(md5, 'digestsize'):
+        digest_size = digestsize
+        del digestsize
+    del md5
 
--- Hash/SHA.py
+++ Hash/SHA.py
@@ -3,9 +3,19 @@
 
 __revision__ = "$Id$"
 
-from sha import *
-import sha
-if hasattr(sha, 'digestsize'):
-    digest_size = digestsize
-    del digestsize
-del sha
+__all__ = ['new', 'digest_size']
+
+try:
+    # The md5 module is deprecated in Python 2.6, so use hashlib when possible.
+    import hashlib
+    def new(data=""):
+        return hashlib.sha1(data)
+    digest_size = new().digest_size
+
+except ImportError:
+    from sha import *
+    import sha
+    if hasattr(sha, 'digestsize'):
+        digest_size = digestsize
+        del digestsize
+    del sha
