fb5f36a21dc8b91c40ee11080eb0b930a3a31898
Dwayne C. Litzenberger <dlitz@dlitz.net>
2008-09-20 17:22:43 -0400

    RSA: Fix _blind() in _slowmath

--- PublicKey/_slowmath.py
+++ PublicKey/_slowmath.py
@@ -41,7 +41,7 @@ class error(Exception):
 class _RSAKey(object):
     def _blind(self, m, r):
         # compute r**e * m (mod n)
-        return pow(r, self.e, n)
+        return m * pow(r, self.e, self.n)
 
     def _unblind(self, m, r):
         # compute m / r (mod n)
