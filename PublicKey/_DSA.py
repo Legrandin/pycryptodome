7119131b460998a7fddefe865bd216947b86322b
Dwayne C. Litzenberger <dlitz@dlitz.net>
2008-10-16 09:51:32 -0400

    DSA: Remove dead code from the _DSA module

--- PublicKey/_DSA.py
+++ PublicKey/_DSA.py
@@ -18,11 +18,6 @@ from Crypto.Util import number
 from Crypto.Util.number import bytes_to_long, long_to_bytes
 from Crypto.Hash import SHA
 
-try:
-    from Crypto.PublicKey import _fastmath
-except ImportError:
-    _fastmath = None
-
 class error (Exception):
     pass
 
@@ -44,7 +39,7 @@ def generateQ(randfunc):
         return S, q
     raise error, 'Bad q value generated'
 
-def generate(bits, randfunc, progress_func=None):
+def generate_py(bits, randfunc, progress_func=None):
     """generate(bits:int, randfunc:callable, progress_func:callable)
 
     Generate a DSA key of length 'bits', using 'randfunc' to get
@@ -100,139 +95,6 @@ def generate(bits, randfunc, progress_func=None):
     obj.x, obj.y = x, pow(g, x, p)
     return obj
 
-def construct(tuple):
-    """construct(tuple:(long,long,long,long)|(long,long,long,long,long)):DSAobj
-    Construct a DSA object from a 4- or 5-tuple of numbers.
-    """
-    obj=DSAobj()
-    if len(tuple) not in [4,5]:
-        raise error, 'argument for construct() wrong length'
-    for i in range(len(tuple)):
-        field = obj.keydata[i]
-        setattr(obj, field, tuple[i])
-    return obj
+class DSAobj:
+    pass
 
-class DSAobj(pubkey):
-    keydata=['y', 'g', 'p', 'q', 'x']
-
-    def _encrypt(self, s, Kstr):
-        raise error, 'DSA algorithm cannot encrypt data'
-
-    def _decrypt(self, s):
-        raise error, 'DSA algorithm cannot decrypt data'
-
-    def _sign(self, M, K):
-        if (K<2 or self.q<=K):
-            raise error, 'K is not between 2 and q'
-        r=pow(self.g, K, self.p) % self.q
-        s=(inverse(K, self.q)*(M+self.x*r)) % self.q
-        return (r,s)
-
-    def _verify(self, M, sig):
-        r, s = sig
-        if r<=0 or r>=self.q or s<=0 or s>=self.q:
-            return 0
-        w=inverse(s, self.q)
-        u1, u2 = (M*w) % self.q, (r*w) % self.q
-        v1 = pow(self.g, u1, self.p)
-        v2 = pow(self.y, u2, self.p)
-        v = ((v1*v2) % self.p)
-        v = v % self.q
-        if v==r:
-            return 1
-        return 0
-
-    def size(self):
-        "Return the maximum number of bits that can be handled by this key."
-        return number.size(self.p) - 1
-
-    def has_private(self):
-        """Return a Boolean denoting whether the object contains
-        private components."""
-        if hasattr(self, 'x'):
-            return 1
-        else:
-            return 0
-
-    def can_sign(self):
-        """Return a Boolean value recording whether this algorithm can generate signatures."""
-        return 1
-
-    def can_encrypt(self):
-        """Return a Boolean value recording whether this algorithm can encrypt data."""
-        return 0
-
-    def publickey(self):
-        """Return a new key object containing only the public information."""
-        return construct((self.y, self.g, self.p, self.q))
-
-object=DSAobj
-
-generate_py = generate
-construct_py = construct
-
-class DSAobj_c(pubkey):
-    keydata = ['y', 'g', 'p', 'q', 'x']
-
-    def __init__(self, key):
-        self.key = key
-
-    def __getattr__(self, attr):
-        if attr in self.keydata:
-            return getattr(self.key, attr)
-        else:
-            if self.__dict__.has_key(attr):
-                self.__dict__[attr]
-            else:
-                raise AttributeError, '%s instance has no attribute %s' % (self.__class__, attr)
-
-    def __getstate__(self):
-        d = {}
-        for k in self.keydata:
-            if hasattr(self.key, k):
-                d[k]=getattr(self.key, k)
-        return d
-
-    def __setstate__(self, state):
-        y,g,p,q = state['y'], state['g'], state['p'], state['q']
-        if not state.has_key('x'):
-            self.key = _fastmath.dsa_construct(y,g,p,q)
-        else:
-            x = state['x']
-            self.key = _fastmath.dsa_construct(y,g,p,q,x)
-
-    def _sign(self, M, K):
-        return self.key._sign(M, K)
-
-    def _verify(self, M, (r, s)):
-        return self.key._verify(M, r, s)
-
-    def size(self):
-        return self.key.size()
-
-    def has_private(self):
-        return self.key.has_private()
-
-    def publickey(self):
-        return construct_c((self.key.y, self.key.g, self.key.p, self.key.q))
-
-    def can_sign(self):
-        return 1
-
-    def can_encrypt(self):
-        return 0
-
-def generate_c(bits, randfunc, progress_func=None):
-    obj = generate_py(bits, randfunc, progress_func)
-    y,g,p,q,x = obj.y, obj.g, obj.p, obj.q, obj.x
-    return construct_c((y,g,p,q,x))
-
-def construct_c(tuple):
-    key = apply(_fastmath.dsa_construct, tuple)
-    return DSAobj_c(key)
-
-if _fastmath:
-    #print "using C version of DSA"
-    generate = generate_c
-    construct = construct_c
-    error = _fastmath.error
