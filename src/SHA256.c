cc6d9927991a4a513c79d77cca6631c1fad0605a
Dwayne C. Litzenberger <dlitz@dlitz.net>
2009-08-02 22:26:52 -0400

    SHA256.c: Replace the legal notice
    I have permission to do this.  See the LEGAL directory.

--- src/SHA256.c
+++ src/SHA256.c
@@ -8,8 +8,29 @@
  *
  * Revised Code:  Complies to SHA-256 standard now.
  *
- * Tom St Denis -- http://tomstdenis.home.dhs.org
- * */
+ * Originally written by Tom St Denis -- http://tomstdenis.home.dhs.org
+ *
+ * Adapted for PyCrypto by Jeethu Rao, Taylor Boon, and others.
+ *
+ * ===================================================================
+ * The contents of this file are dedicated to the public domain.  To
+ * the extent that dedication to the public domain is not available,
+ * everyone is granted a worldwide, perpetual, royalty-free,
+ * non-exclusive license to exercise all rights associated with the
+ * contents of this file for any purpose whatsoever.
+ * No rights are reserved.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ===================================================================
+ *
+ */
 #include "Python.h"
 #define MODULE_NAME SHA256
 #define DIGEST_SIZE 32
