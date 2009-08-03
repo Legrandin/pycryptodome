6e4f9c960402b45fba8528eeec250faa5f437171
Dwayne C. Litzenberger <dlitz@dlitz.net>
2009-08-03 16:18:22 -0400

    Chaffing.py, test_chaffing.py: Add/Replace legal notices
    I have permission to do this.  See the LEGAL directory.

--- lib/Crypto/SelfTest/Protocol/test_chaffing.py
+++ lib/Crypto/SelfTest/Protocol/test_chaffing.py
@@ -1,7 +1,27 @@
 #
 # Test script for Crypto.Protocol.Chaffing
 #
-# Part of PyCrypto 2.0.1; Presumably written by A. M. Kuchling.
+# Part of the Python Cryptography Toolkit
+#
+# Written by Andrew Kuchling and others
+#
+# ===================================================================
+# The contents of this file are dedicated to the public domain.  To
+# the extent that dedication to the public domain is not available,
+# everyone is granted a worldwide, perpetual, royalty-free,
+# non-exclusive license to exercise all rights associated with the
+# contents of this file for any purpose whatsoever.
+# No rights are reserved.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ===================================================================
 
 __revision__ = "$Id$"
 
