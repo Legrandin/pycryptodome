20cf8add93ccd6f4e4dc60691585256ba36e7810
Dwayne Litzenberger <dlitz@dlitz.net>
2013-02-16 11:09:31 -0800

    asn1: Python 2.1 compatibility

--- lib/Crypto/Util/asn1.py
+++ lib/Crypto/Util/asn1.py
@@ -22,6 +22,8 @@
 
 from Crypto.Util.number import long_to_bytes, bytes_to_long
 import sys
+if sys.version_info[0] == 2 and sys.version_info[1] == 1:
+    from Crypto.Util.py21compat import *
 from Crypto.Util.py3compat import *
 
 __all__ = [ 'DerObject', 'DerInteger', 'DerOctetString', 'DerNull', 'DerSequence', 'DerObjectId' ]
