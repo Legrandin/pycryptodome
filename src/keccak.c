db9a3077d89d5e06ada7e3835e3f9ea9b6a6df5d
Fabrizio Tarizzo <fabrizio@fabriziotarizzo.org>
2013-04-22 10:19:42 +0200

    Some optimization 	modified:   src/keccak.c

--- src/keccak.c
+++ src/keccak.c
@@ -268,6 +268,8 @@ keccak_function (uint64_t *state)
     
     uint64_t b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12;
     uint64_t b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24;
+    
+    uint64_t c0, c1, c2, c3, c4, d;
 
     a0  = state[0];
     a1  = state[1];
@@ -294,113 +296,87 @@ keccak_function (uint64_t *state)
     a22 = state[22];
     a23 = state[23];
     a24 = state[24];
-    
+        
     for (i = 0; i < KECCAK_ROUNDS; ++i) {
         /*
            Uses temporary variables and loop unrolling to
            avoid array indexing and inner loops overhead
         */
-    
-        /* Theta step */
-        b0 = a0 ^ a5 ^ a10 ^ a15 ^ a20;
-        b1 = a1 ^ a6 ^ a11 ^ a16 ^ a21;
-        b2 = a2 ^ a7 ^ a12 ^ a17 ^ a22;
-        b3 = a3 ^ a8 ^ a13 ^ a18 ^ a23;  
-        b4 = a4 ^ a9 ^ a14 ^ a19 ^ a24;
         
-        b5   = b4 ^ ROL64(b1, 1);
-        a0  ^= b5;
-        a5  ^= b5;
-        a10 ^= b5;
-        a15 ^= b5;
-        a20 ^= b5;
+        /* Prepare column parity for Theta step */
+        c0 = a0 ^ a5 ^ a10 ^ a15 ^ a20;
+        c1 = a1 ^ a6 ^ a11 ^ a16 ^ a21;
+        c2 = a2 ^ a7 ^ a12 ^ a17 ^ a22;
+        c3 = a3 ^ a8 ^ a13 ^ a18 ^ a23;  
+        c4 = a4 ^ a9 ^ a14 ^ a19 ^ a24;
         
-        b5   = b0 ^ ROL64(b2, 1);        
-        a1  ^= b5;
-        a6  ^= b5;
-        a11 ^= b5;
-        a16 ^= b5;
-        a21 ^= b5;
+        /* Theta + Rho + Pi steps */
+        d   = c4 ^ ROL64(c1, 1);
+        b0  = d ^ a0;
+        b16 = ROL64(d ^ a5,  ROT_01);       
+        b7  = ROL64(d ^ a10, ROT_02);
+        b23 = ROL64(d ^ a15, ROT_03);
+        b14 = ROL64(d ^ a20, ROT_04);
         
-        b5   = b1 ^ ROL64(b3, 1);
-        a2  ^= b5;
-        a7  ^= b5;
-        a12 ^= b5;
-        a17 ^= b5;
-        a22 ^= b5;
+        d   = c0 ^ ROL64(c2, 1);
+        b10 = ROL64(d ^ a1,  ROT_05);                       
+        b1  = ROL64(d ^ a6,  ROT_06);
+        b17 = ROL64(d ^ a11, ROT_07);
+        b8  = ROL64(d ^ a16, ROT_08);
+        b24 = ROL64(d ^ a21, ROT_09);
         
-        b5   = b2 ^ ROL64(b4, 1);
-        a3  ^= b5;
-        a8  ^= b5;
-        a13 ^= b5;
-        a18 ^= b5;
-        a23 ^= b5;
-                
-        b5   = b3 ^ ROL64(b0, 1);
-        a4  ^= b5;
-        a9  ^= b5;
-        a14 ^= b5;
-        a19 ^= b5;
-        a24 ^= b5;
+        d   = c1 ^ ROL64(c3, 1);
+        b20 = ROL64(d ^ a2,  ROT_10);
+        b11 = ROL64(d ^ a7,  ROT_11);            
+        b2  = ROL64(d ^ a12, ROT_12);
+        b18 = ROL64(d ^ a17, ROT_13);
+        b9  = ROL64(d ^ a22, ROT_14);
         
-        /* Rho + Pi steps */
-        b0  = a0;
-        b1  = ROL64(a6,  ROT_06);
-        b2  = ROL64(a12, ROT_12);
-        b3  = ROL64(a18, ROT_18);
-        b4  = ROL64(a24, ROT_24);
-        b5  = ROL64(a3,  ROT_15);
-        b6  = ROL64(a9,  ROT_21);
-        b7  = ROL64(a10, ROT_02);
-        b8  = ROL64(a16, ROT_08);
-        b9  = ROL64(a22, ROT_14);
-        b10 = ROL64(a1,  ROT_05);
-        b11 = ROL64(a7,  ROT_11);
-        b12 = ROL64(a13, ROT_17);
-        b13 = ROL64(a19, ROT_23);
-        b14 = ROL64(a20, ROT_04);
-        b15 = ROL64(a4,  ROT_20);
-        b16 = ROL64(a5,  ROT_01);
-        b17 = ROL64(a11, ROT_07);
-        b18 = ROL64(a17, ROT_13);
-        b19 = ROL64(a23, ROT_19);
-        b20 = ROL64(a2,  ROT_10);
-        b21 = ROL64(a8,  ROT_16);
-        b22 = ROL64(a14, ROT_22);
-        b23 = ROL64(a15, ROT_03);
-        b24 = ROL64(a21, ROT_09);
+        d   = c2 ^ ROL64(c4, 1);
+        b5  = ROL64(d ^ a3,  ROT_15);  
+        b21 = ROL64(d ^ a8,  ROT_16);
+        b12 = ROL64(d ^ a13, ROT_17);                      
+        b3  = ROL64(d ^ a18, ROT_18);
+        b19 = ROL64(d ^ a23, ROT_19);
         
+        d   = c3 ^ ROL64(c0, 1);
+        b15 = ROL64(d ^ a4,  ROT_20);
+        b6  = ROL64(d ^ a9,  ROT_21);
+        b22 = ROL64(d ^ a14, ROT_22);
+        b13 = ROL64(d ^ a19, ROT_23);
+        b4  = ROL64(d ^ a24, ROT_24);
+
         /* Chi + Iota steps */
 #ifdef USE_COMPLEMENT_LANES_OPTIMIZATION
-        a0 =   b0 ^ ( b1 | b2 ) ^ roundconstants[i];
-        a1 =   b1 ^ (~b2 | b3 );
-        a2 =   b2 ^ ( b3 & b4 );
-        a3 =   b3 ^ ( b4 | b0 );
-        a4 =   b4 ^ ( b0 & b1 );
+        a0 = b0 ^ ( b1 | b2) ^ roundconstants[i];
+        a1 = b1 ^ (~b2 | b3);
+        a2 = b2 ^ ( b3 & b4);
+        a3 = b3 ^ ( b4 | b0);
+        a4 = b4 ^ ( b0 & b1);
         
-        a5 =   b5 ^ ( b6 |  b7 );
-        a6 =   b6 ^ ( b7 &  b8 );
-        a7 =   b7 ^ ( b8 | ~b9);
-        a8 =   b8 ^ ( b9 |  b5 );
-        a9 =   b9 ^ ( b5 &  b6 );
+        a5 = b5 ^ ( b6 |  b7);
+        a6 = b6 ^ ( b7 &  b8);
+        a7 = b7 ^ ( b8 | ~b9);
+        a8 = b8 ^ ( b9 |  b5);
+        a9 = b9 ^ ( b5 &  b6);
         
-        a10 =   b10 ^ ( b11 |  b12 );
-        a11 =   b11 ^ ( b12 &  b13 );
-        a12 =   b12 ^ (~b13 &  b14 );
-        a13 =  ~b13 ^ ( b14 |  b10 );
-        a14 =   b14 ^ ( b10 &  b11 );
+        a10 =  b10 ^ ( b11 |  b12);
+        a11 =  b11 ^ ( b12 &  b13);
+        a12 =  b12 ^ (~b13 &  b14);
+        a13 = ~b13 ^ ( b14 |  b10);
+        a14 =  b14 ^ ( b10 &  b11);
         
-        a15 =   b15 ^ ( b16 & b17 );
-        a16 =   b16 ^ ( b17 | b18 );
-        a17 =   b17 ^ (~b18 | b19 );
-        a18 =  ~b18 ^ ( b19 & b15 );
-        a19 =   b19 ^ ( b15 | b16 );
+        a15 =  b15 ^ ( b16 & b17);
+        a16 =  b16 ^ ( b17 | b18);
+        a17 =  b17 ^ (~b18 | b19);
+        a18 = ~b18 ^ ( b19 & b15);
+        a19 =  b19 ^ ( b15 | b16);
         
-        a20 =   b20 ^ (~b21 & b22 );
-        a21 =  ~b21 ^ ( b22 | b23 );
-        a22 =   b22 ^ ( b23 & b24 );
-        a23 =   b23 ^ ( b24 | b20 );
-        a24 =   b24 ^ ( b20 & b21 );
+        a20 =  b20 ^ (~b21 & b22);
+        a21 = ~b21 ^ ( b22 | b23);
+        a22 =  b22 ^ ( b23 & b24);
+        a23 =  b23 ^ ( b24 | b20);
+        a24 =  b24 ^ ( b20 & b21);
 #else
         a0  = b0  ^ (~b1  & b2) ^ roundconstants[i];
         a1  = b1  ^ (~b2  & b3);
