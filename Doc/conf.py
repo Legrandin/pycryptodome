a5fa43dfd1c97efa5897d7f0f52a59a1f58e531f
Legrandin <helderijs@gmail.com>
2014-06-18 21:29:04 +0200

    Switch to haiku theme for documentation.

--- Doc/conf.py
+++ Doc/conf.py
@@ -97,12 +97,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+        "textcolor" : "#000000",
+        "headingcolor" : "#000000",
+        "linkcolor" : "#FF0000"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
@@ -159,7 +163,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
