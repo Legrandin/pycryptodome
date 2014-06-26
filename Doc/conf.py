462ffd056569a9878c73f25992c2c3319523bedc
Legrandin <helderijs@gmail.com>
2014-06-26 22:39:31 +0200

    Add github badge to documentation

--- Doc/conf.py
+++ Doc/conf.py
@@ -102,7 +102,10 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+        "github_user" : "Legrandin",
+        "github_repo" : "pycryptodome"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = [ "." ]
