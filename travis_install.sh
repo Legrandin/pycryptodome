bb8e8657a9251e6a05df91d1024aa7a788ea7d79
Legrandin <helderijs@gmail.com>
2014-06-29 21:52:29 +0200

    Introduce additional Python implementations
    Add all CPythons from 2.4 to 3.4, exluded 3.0.
    Add the most recent PyPy build.

---
+++ travis_install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get install libgmp-dev
+
+if [ x${PYTHON_INTP} = "x" ]; then
+	echo "Undefined python implementation"
+	exit 1
+fi
+
+if [ ${PYTHON_INTP} = "pypy" ]; then
+	sudo add-apt-repository -y ppa:pypy/ppa
+	sudo apt-get -y update
+	sudo apt-get install -y --force-yes pypy pypy-dev
+elif [ x$(which ${PYTHON_INTP}) = "x" ]; then
+	sudo add-apt-repository -y ppa:fkrull/deadsnakes
+	sudo apt-get -y update
+	sudo apt-get install ${PYTHON_INTP} ${PYTHON_INTP}-dev
+fi
+
+${PYTHON_INTP} -V
+
+PYV=$(${PYTHON_INTP} -V 2>&1 | head -1 | sed -n 's/\w\+ \([23]\)\.\([0-9]\).*/\1\2/p')
+
+if [ ${PYV} -ge 26 ]; then
+	sudo pip install virtualenv
+else
+	sudo apt-get install python-virtualenv
+fi
+
+virtualenv -p ${PYTHON_INTP} .
