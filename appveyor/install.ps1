dd5360afa1d1ca9ef05763a74d988b3d9b5b9b88
Legrandin <helderijs@gmail.com>
2015-02-17 22:10:49 +0100

    Download MPIR from github

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -5,7 +5,7 @@
 $BASE_URL = "https://www.python.org/ftp/python/"
 $GET_PIP_URL = "https://bootstrap.pypa.io/get-pip.py"
 $GET_PIP_PATH = "C:\get-pip.py"
-
+$MPIR_GITHUB = "https://github.com/Legrandin/mpir-windows-builds/raw/master/"
 
 function DownloadPython ($python_version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
@@ -76,10 +76,24 @@ function InstallPackage ($python_home, $pkg) {
     & $pip_path install $pkg
 }
 
+function InstallMPIR ($python_version, $architecture, $python_home) {
+    $pyver = $python_version.split(".")
+    if ($pyver[0] -gt 2) {
+      $vs = "VS2010"
+    } else {
+      $vs = "VS2008"
+    }
+    $download_uri = $MPIR_GITHUB + "mpir-2.6.0_" + $vs + "_" + $architecture + "/mpir.dll"
+    Write-Host "Downloading MPIR from " $download_uri
+    $webclient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($download_uri, $python_home + "\mpir.dll")
+}
+
 function main () {
     InstallPython $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     InstallPip $env:PYTHON
     InstallPackage $env:PYTHON wheel
+    InstallMPIR $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
 }
 
 main
