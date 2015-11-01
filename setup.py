61fffea89a19a4985381e9d490c216a4d948f962
Legrandin <helderijs@gmail.com>
2015-11-01 06:22:21 -0500

    Update to metadata in setup.py

--- setup.py
+++ setup.py
@@ -300,6 +300,9 @@ setup(
         'Topic :: Security :: Cryptography',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ],
     packages =  [
