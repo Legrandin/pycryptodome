6e41d35fb3adbdfc504e42aba37313b563b10e7a
Legrandin <helderijs@gmail.com>
2015-11-28 21:31:40 +0100

    Reimplement ARC2
    ARC2 is only interesting for historical reasons, and it should
    not be used for new designs. The current implementation trips PyPy4
    which may indicate a bug: a cleaner rewriting based on RFC2268
    is the quickest solution.

--- lib/Crypto/Cipher/ARC2.py
+++ lib/Crypto/Cipher/ARC2.py
@@ -100,8 +100,9 @@ def _create_base_cipher(dict_parameters):
     if len(key) not in key_size:
         raise ValueError("Incorrect ARC2 key length (%d bytes)" % len(key))
 
-    if not (0 <= effective_keylen <= 1024):
-        raise ValueError("'effective_key_len' must be positive and no larger than 1024")
+    if not (0 < effective_keylen <= 1024):
+        raise ValueError("'effective_key_len' must be no larger than 1024 "
+                         "(not %d)" % effective_keylen)
 
     start_operation = _raw_arc2_lib.ARC2_start_operation
     stop_operation = _raw_arc2_lib.ARC2_stop_operation
--- src/ARC2.c
+++ src/ARC2.c
@@ -1,44 +1,32 @@
-/*
- *  rc2.c : Source code for the RC2 block cipher
+/* ===================================================================
  *
- * Part of the Python Cryptography Toolkit
+ * Copyright (c) 2015, Legrandin <helderijs@gmail.com>
+ * All rights reserved.
  *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  * ===================================================================
- * This file appears to contain code from the ARC2 implementation
- * "rc2.c" implementation (the "Original Code"), with modifications made
- * after it was incorporated into PyCrypto (the "Modifications").
- *
- * To the best of our knowledge, the Original Code was placed into the
- * public domain by its (anonymous) author:
- *
- *  **********************************************************************
- * * To commemorate the 1996 RSA Data Security Conference, the following  *
- * * code is released into the public domain by its author.  Prost!       *
- * *                                                                      *
- * * This cipher uses 16-bit words and little-endian byte ordering.       *
- * * I wonder which processor it was optimized for?                       *
- * *                                                                      *
- * * Thanks to CodeView, SoftIce, and D86 for helping bring this code to  *
- * * the public.                                                          *
- *  **********************************************************************
- *
- * The Modifications to this file are dedicated to the public domain.
- * To the extent that dedication to the public domain is not available,
- * everyone is granted a worldwide, perpetual, royalty-free,
- * non-exclusive license to exercise all rights associated with the
- * contents of this file for any purpose whatsoever.  No rights are
- * reserved.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
- * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * ===================================================================
- *
  */
 
 #include "pycrypto_common.h"
@@ -49,173 +37,159 @@ FAKE_INIT(raw_arc2)
 #define MODULE_NAME ARC2
 #define BLOCK_SIZE 8
 #define KEY_SIZE 0
-#define PCT_ARC2_MODULE  /* Defined to get ARC2's additional keyword arguments */
-
-typedef uint32_t U32;
-typedef uint16_t U16;
-typedef uint8_t U8;
 
 typedef struct
 {
-	U16 xkey[64];
-        int effective_keylen;
+    uint16_t exp_key[64];
 } block_state;
 
-static void
-block_encrypt(block_state *self, U8 *in, U8 *out)
-{
-	U16 x76, x54, x32, x10;
-	int i;
-
-	x76 = (in[7] << 8) + in[6];
-	x54 = (in[5] << 8) + in[4];
-	x32 = (in[3] << 8) + in[2];
-	x10 = (in[1] << 8) + in[0];
-
-	for (i = 0; i < 16; i++)
-	{
-		x10 += (x32 & ~x76) + (x54 & x76) + self->xkey[4*i+0];
-		x10 = (x10 << 1) + (x10 >> 15 & 1);
-
-		x32 += (x54 & ~x10) + (x76 & x10) + self->xkey[4*i+1];
-		x32 = (x32 << 2) + (x32 >> 14 & 3);
-
-		x54 += (x76 & ~x32) + (x10 & x32) + self->xkey[4*i+2];
-		x54 = (x54 << 3) + (x54 >> 13 & 7);
-
-		x76 += (x10 & ~x54) + (x32 & x54) + self->xkey[4*i+3];
-		x76 = (x76 << 5) + (x76 >> 11 & 31);
-
-		if (i == 4 || i == 10) {
-			x10 += self->xkey[x76 & 63];
-			x32 += self->xkey[x10 & 63];
-			x54 += self->xkey[x32 & 63];
-			x76 += self->xkey[x54 & 63];
-		}
-	}
-
-	out[0] = (U8)x10;
-	out[1] = (U8)(x10 >> 8);
-	out[2] = (U8)x32;
-	out[3] = (U8)(x32 >> 8);
-	out[4] = (U8)x54;
-	out[5] = (U8)(x54 >> 8);
-	out[6] = (U8)x76;
-	out[7] = (U8)(x76 >> 8);
-}
-
-
-static void
-block_decrypt(block_state *self, U8 *in, U8 *out)
-{
-	U16 x76, x54, x32, x10;
-	int i;
-
-	x76 = (in[7] << 8) + in[6];
-	x54 = (in[5] << 8) + in[4];
-	x32 = (in[3] << 8) + in[2];
-	x10 = (in[1] << 8) + in[0];
-
-	i = 15;
-	do {
-		x76 &= 65535;
-		x76 = (x76 << 11) + (x76 >> 5);
-		x76 -= (x10 & ~x54) + (x32 & x54) + self->xkey[4*i+3];
-
-		x54 &= 65535;
-		x54 = (x54 << 13) + (x54 >> 3);
-		x54 -= (x76 & ~x32) + (x10 & x32) + self->xkey[4*i+2];
-
-		x32 &= 65535;
-		x32 = (x32 << 14) + (x32 >> 2);
-		x32 -= (x54 & ~x10) + (x76 & x10) + self->xkey[4*i+1];
-
-		x10 &= 65535;
-		x10 = (x10 << 15) + (x10 >> 1);
-		x10 -= (x32 & ~x76) + (x54 & x76) + self->xkey[4*i+0];
-
-		if (i == 5 || i == 11) {
-			x76 -= self->xkey[x54 & 63];
-			x54 -= self->xkey[x32 & 63];
-			x32 -= self->xkey[x10 & 63];
-			x10 -= self->xkey[x76 & 63];
-		}
-	} while (i--);
-
-	out[0] = (U8)x10;
-	out[1] = (U8)(x10 >> 8);
-	out[2] = (U8)x32;
-	out[3] = (U8)(x32 >> 8);
-	out[4] = (U8)x54;
-	out[5] = (U8)(x54 >> 8);
-	out[6] = (U8)x76;
-	out[7] = (U8)(x76 >> 8);
-}
-
-
 static int
-block_init(block_state *self, U8 *key, int keylength)
+block_init(block_state *self, const uint8_t *key, size_t t /* key_bytes */,
+           size_t effective_key_bits)
 {
-	U8 x;
-	U16 i;
-        /* 256-entry permutation table, probably derived somehow from pi */
-        static const U8 permute[256] = {
-		217,120,249,196, 25,221,181,237, 40,233,253,121, 74,160,216,157,
-		198,126, 55,131, 43,118, 83,142, 98, 76,100,136, 68,139,251,162,
-		23,154, 89,245,135,179, 79, 19, 97, 69,109,141,  9,129,125, 50,
-		189,143, 64,235,134,183,123, 11,240,149, 33, 34, 92,107, 78,130,
-		84,214,101,147,206, 96,178, 28,115, 86,192, 20,167,140,241,220,
-		18,117,202, 31, 59,190,228,209, 66, 61,212, 48,163, 60,182, 38,
-		111,191, 14,218, 70,105,  7, 87, 39,242, 29,155,188,148, 67,  3,
-		248, 17,199,246,144,239, 62,231,  6,195,213, 47,200,102, 30,215,
-		8,232,234,222,128, 82,238,247,132,170,114,172, 53, 77,106, 42,
-		150, 26,210,113, 90, 21, 73,116, 75,159,208, 94,  4, 24,164,236,
-		194,224, 65,110, 15, 81,203,204, 36,145,175, 80,161,244,112, 57,
-		153,124, 58,133, 35,184,180,122,252,  2, 54, 91, 37, 85,151, 49,
-		45, 93,250,152,227,138,146,174,  5,223, 41, 16,103,108,186,201,
-		211,  0,230,207,225,158,168, 44, 99, 22,  1, 63, 88,226,137,169,
-		13, 56, 52, 27,171, 51,255,176,187, 72, 12, 95,185,177,205, 46,
-		197,243,219, 71,229,165,156,119, 10,166, 32,104,254,127,193,173
-        };
+    uint8_t t8, tm;
+    int i;
+    uint8_t bkey[128];
 
-	if ((U32)keylength > sizeof(self->xkey)) {
-            return ERR_KEY_SIZE;
-	}
+    static const uint8_t permute[256] = {
+	217,120,249,196, 25,221,181,237, 40,233,253,121, 74,160,216,157,
+	198,126, 55,131, 43,118, 83,142, 98, 76,100,136, 68,139,251,162,
+	23,154, 89,245,135,179, 79, 19, 97, 69,109,141,  9,129,125, 50,
+	189,143, 64,235,134,183,123, 11,240,149, 33, 34, 92,107, 78,130,
+	84,214,101,147,206, 96,178, 28,115, 86,192, 20,167,140,241,220,
+	18,117,202, 31, 59,190,228,209, 66, 61,212, 48,163, 60,182, 38,
+	111,191, 14,218, 70,105,  7, 87, 39,242, 29,155,188,148, 67,  3,
+	248, 17,199,246,144,239, 62,231,  6,195,213, 47,200,102, 30,215,
+	8,232,234,222,128, 82,238,247,132,170,114,172, 53, 77,106, 42,
+	150, 26,210,113, 90, 21, 73,116, 75,159,208, 94,  4, 24,164,236,
+	194,224, 65,110, 15, 81,203,204, 36,145,175, 80,161,244,112, 57,
+	153,124, 58,133, 35,184,180,122,252,  2, 54, 91, 37, 85,151, 49,
+	45, 93,250,152,227,138,146,174,  5,223, 41, 16,103,108,186,201,
+	211,  0,230,207,225,158,168, 44, 99, 22,  1, 63, 88,226,137,169,
+	13, 56, 52, 27,171, 51,255,176,187, 72, 12, 95,185,177,205, 46,
+	197,243,219, 71,229,165,156,119, 10,166, 32,104,254,127,193,173
+    };
 
-	memcpy(self->xkey, key, keylength);
+    if (NULL == self)
+        return ERR_NULL;
 
-	/* Phase 1: Expand input key to 128 bytes */
-	if (keylength < 128) {
-		i = 0;
-		x = ((U8 *)self->xkey)[keylength-1];
-                do {
-			x = permute[(x + ((U8 *)self->xkey)[i++]) & 255];
-                        ((U8 *)self->xkey)[keylength++] = x;
-                } while (keylength < 128);
-	}
+    if ((t < 1) || (t > 128))
+        return ERR_KEY_SIZE;
 
-	/* Phase 2 - reduce effective key size to "effective_keylen" */
-        keylength = (self->effective_keylen+7) >> 3;
-	i = 128-keylength;
-	x = permute[((U8 *)self->xkey)[i] & (255 >>
-					     (7 &
-					      ((self->effective_keylen %8 ) ? 8-(self->effective_keylen%8): 0))
-		)];
-	((U8 *)self->xkey)[i] = x;
+    if ((effective_key_bits < 1) || (effective_key_bits > 1024))
+        return ERR_KEY_SIZE;
 
-	while (i--) {
-		x = permute[ x ^ ((U8 *)self->xkey)[i+keylength] ];
-		((U8 *)self->xkey)[i] = x;
-        }
+    memcpy(bkey, key, t);
 
-	/* Phase 3 - copy to self->xkey in little-endian order */
-	i = 63;
-	do {
-		self->xkey[i] =  ((U8 *)self->xkey)[2*i] +
-			(((U8 *)self->xkey)[2*i+1] << 8);
-	} while (i--);
+    t8 = (effective_key_bits + 7) / 8;
+    tm = (1 << (8 - (t8*8 - effective_key_bits))) - 1;
 
-        return 0;
+    for (i=t; i<128; i++)
+        bkey[i] = permute[(bkey[i-1] + bkey[i-t]) % 256];
+    
+    bkey[128-t8] = permute[bkey[128-t8] & tm];
+
+    for (i=127-t8; i>=0; i--)
+        bkey[i] = permute[bkey[i+1] ^ bkey[i+t8]];
+
+    for (i=0; i<64; i++)
+        self->exp_key[i] = bkey[2*i] + 256*bkey[2*i+1];
+
+    return 0;
+}
+
+#define ROL16(x, p) ((((x) << (p)) | ((x) >> (16-(p)))) & 0xFFFF)
+#define ROR16(x, p) ((((x) >> (p)) | ((x) << (16-(p)))) & 0xFFFF)
+
+static inline void mix_round(uint16_t *r, const uint16_t *k, size_t *j)
+{
+    r[0] += k[(*j)++] + (r[3] & r[2]) + (~r[3] & r[1]);
+    r[0] = ROL16(r[0], 1);
+    r[1] += k[(*j)++] + (r[0] & r[3]) + (~r[0] & r[2]);
+    r[1] = ROL16(r[1], 2);
+    r[2] += k[(*j)++] + (r[1] & r[0]) + (~r[1] & r[3]);
+    r[2] = ROL16(r[2], 3);
+    r[3] += k[(*j)++] + (r[2] & r[1]) + (~r[2] & r[0]);
+    r[3] = ROL16(r[3], 5);
+}
+
+static inline void inv_mix_round(uint16_t *r, const uint16_t *k, size_t *j)
+{
+    r[3] = ROR16(r[3], 5);
+    r[3] -= k[(*j)--] + (r[2] & r[1]) + (~r[2] & r[0]);
+    r[2] = ROR16(r[2], 3);
+    r[2] -= k[(*j)--] + (r[1] & r[0]) + (~r[1] & r[3]);
+    r[1] = ROR16(r[1], 2);
+    r[1] -= k[(*j)--] + (r[0] & r[3]) + (~r[0] & r[2]);
+    r[0] = ROR16(r[0], 1);
+    r[0] -= k[(*j)--] + (r[3] & r[2]) + (~r[3] & r[1]);
+}
+
+static inline void mash_round(uint16_t *r, const uint16_t *k)
+{
+    r[0] += k[r[3] & 63];
+    r[1] += k[r[0] & 63];
+    r[2] += k[r[1] & 63];
+    r[3] += k[r[2] & 63];
+}
+
+static inline void inv_mash_round(uint16_t *r, const uint16_t *k)
+{
+    r[3] -= k[r[2] & 63];
+    r[2] -= k[r[1] & 63];
+    r[1] -= k[r[0] & 63];
+    r[0] -= k[r[3] & 63];
+}
+
+static void block_encrypt(const block_state *self, const uint8_t *in, uint8_t *out)
+{
+    uint16_t r[4];
+    const uint16_t *k;
+    size_t i, j;
+
+    k = self->exp_key;
+    j = 0;
+
+    for (i=0; i<4; i++) {
+        r[i] = in[2*i] + 256*in[2*i+1];
+    }
+
+    for (i=0; i<5; i++) mix_round(r, k, &j);
+    mash_round(r, k);
+    for (i=0; i<6; i++) mix_round(r, k, &j);
+    mash_round(r, k);
+    for (i=0; i<5; i++) mix_round(r, k, &j);
+    
+    
+    for (i=0; i<4; i++) {
+        out[2*i] = r[i] & 255;
+        out[2*i+1] = r[i] >> 8;
+    }
+}
+
+static void block_decrypt(const block_state *self, const uint8_t *in, uint8_t *out)
+{
+    uint16_t r[4];
+    const uint16_t *k;
+    size_t i, j;
+
+    k = self->exp_key;
+
+    for (i=0; i<4; i++) {
+        r[i] = in[2*i] + 256*in[2*i+1];
+    }
+
+    j = 63;
+    for (i=0; i<5; i++) inv_mix_round(r, k, &j);
+    inv_mash_round(r, k);
+    for (i=0; i<6; i++) inv_mix_round(r, k, &j);
+    inv_mash_round(r, k);
+    for (i=0; i<5; i++) inv_mix_round(r, k, &j);
+    
+    for (i=0; i<4; i++) {
+        out[2*i] = r[i] & 255;
+        out[2*i+1] = r[i] >> 8;
+    }
 }
 
 static void
@@ -243,8 +217,7 @@ EXPORT_SYM int ARC2_start_operation(const uint8_t key[], size_t key_len, size_t 
     block_base->destructor = &ARC2_stop_operation;
     block_base->block_len = BLOCK_SIZE;
 
-    (*pResult)->algo_state.effective_keylen = effective_key_len;
-
-    return block_init(&(*pResult)->algo_state, (unsigned char*)key, key_len);
+    return block_init(&(*pResult)->algo_state, (unsigned char*)key,
+           key_len, effective_key_len);
 }
 
