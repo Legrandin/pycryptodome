052ea1fcde68d45906df9f14e2c42f2701c617ca
Legrandin <helderijs@gmail.com>
2016-01-26 08:04:11 +0100

    Add ECC P-256 X.509 test cert and OpenSSH test key

--- lib/Crypto/SelfTest/PublicKey/test_vectors/ECC/gen_ecc_p256.sh
+++ lib/Crypto/SelfTest/PublicKey/test_vectors/ECC/gen_ecc_p256.sh
@@ -16,3 +16,11 @@ openssl ec -in ecc_p256_private.pem -des3 -out ecc_p256_private_enc.pem -passout
 # Public key
 openssl ec -in ecc_p256_private.pem -pubout -out ecc_p256_public.pem
 openssl ec -pubin -in ecc_p256_public.pem -outform DER -out ecc_p256_public.der
+
+# X.509 cert
+openssl req -new -key ecc_p256_private.pem -days 365 -x509 -out ecc_p256_x509.pem -subj '/C=GB/CN=example.com'
+openssl x509 -in ecc_p256_x509.pem -out ecc_p256_x509.der -outform DER
+
+# OpenSSH
+chmod 600 ecc_p256_private.pem
+ssh-keygen -f ecc_p256_private.pem -y > ecc_p256_public_openssh.txt
