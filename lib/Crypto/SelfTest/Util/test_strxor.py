0914cc686dd930bd8f31c7d4ea810ad1b63c1586
Helder Eijs <helderijs@gmail.com>
2018-04-01 21:52:38 +0200

    Bytearray/memoryview tests for Crypto.Util.strxor

--- lib/Crypto/SelfTest/Util/test_strxor.py
+++ lib/Crypto/SelfTest/Util/test_strxor.py
@@ -34,7 +34,6 @@
 import unittest
 from binascii import unhexlify, hexlify
 
-from Crypto.Util.py3compat import *
 from Crypto.SelfTest.st_common import list_test_cases
 from Crypto.Util.strxor import strxor, strxor_c
 
@@ -42,45 +41,63 @@ from Crypto.Util.strxor import strxor, strxor_c
 class StrxorTests(unittest.TestCase):
 
     def test1(self):
-        term1 = unhexlify(b("ff339a83e5cd4cdf5649"))
-        term2 = unhexlify(b("383d4ba020573314395b"))
-        result = unhexlify(b("c70ed123c59a7fcb6f12"))
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
+        term2 = unhexlify(b"383d4ba020573314395b")
+        result = unhexlify(b"c70ed123c59a7fcb6f12")
         self.assertEqual(strxor(term1, term2), result)
         self.assertEqual(strxor(term2, term1), result)
 
     def test2(self):
-        es = b("")
+        es = b""
         self.assertEqual(strxor(es, es), es)
 
     def test3(self):
-        term1 = unhexlify(b("ff339a83e5cd4cdf5649"))
-        all_zeros = bchr(0) * len(term1)
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
+        all_zeros = b"\x00" * len(term1)
         self.assertEqual(strxor(term1, term1), all_zeros)
 
     def test_wrong_length(self):
-        term1 = unhexlify(b("ff339a83e5cd4cdf5649"))
-        term2 = unhexlify(b("ff339a83e5cd4cdf564990"))
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
+        term2 = unhexlify(b"ff339a83e5cd4cdf564990")
         self.assertRaises(ValueError, strxor, term1, term2)
 
+    def test_bytearray_memoryview(self):
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
+        term1_ba = bytearray(term1)
+        term1_mv = memoryview(term1)
+        term2 = unhexlify(b"383d4ba020573314395b")
+        result = unhexlify(b"c70ed123c59a7fcb6f12")
+
+        self.assertEqual(strxor(term1_ba, term2), result)
+        self.assertEqual(strxor(term1_mv, term2), result)
 
 class Strxor_cTests(unittest.TestCase):
 
     def test1(self):
-        term1 = unhexlify(b("ff339a83e5cd4cdf5649"))
-        result = unhexlify(b("be72dbc2a48c0d9e1708"))
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
+        result = unhexlify(b"be72dbc2a48c0d9e1708")
         self.assertEqual(strxor_c(term1, 65), result)
 
     def test2(self):
-        term1 = unhexlify(b("ff339a83e5cd4cdf5649"))
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
         self.assertEqual(strxor_c(term1, 0), term1)
 
     def test3(self):
-        self.assertEqual(strxor_c(b(""), 90), b(""))
+        self.assertEqual(strxor_c(b"", 90), b"")
 
     def test_wrong_range(self):
-        term1 = unhexlify(b("ff339a83e5cd4cdf5649"))
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
         self.assertRaises(ValueError, strxor_c, term1, -1)
         self.assertRaises(ValueError, strxor_c, term1, 256)
+
+    def test_bytearray_memoryview(self):
+        term1 = unhexlify(b"ff339a83e5cd4cdf5649")
+        term1_ba = bytearray(term1)
+        term1_mv = memoryview(term1)
+        result = unhexlify(b"be72dbc2a48c0d9e1708")
+
+        self.assertEqual(strxor_c(term1_ba, 65), result)
+        self.assertEqual(strxor_c(term1_mv, 65), result)
 
 def get_tests(config={}):
     tests = []
