845281fb9e88457dae1e596b9c632d97e199512b
Helder Eijs <helderijs@gmail.com>
2018-11-24 21:47:45 +0100

    Bump version to 3.7.1

--- lib/Crypto/__init__.py
+++ lib/Crypto/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['Cipher', 'Hash', 'Protocol', 'PublicKey', 'Util', 'Signature',
            'IO', 'Math']
 
-version_info = (3, 7, '1a0')
+version_info = (3, 7, '1')
 
 __version__ = ".".join([str(x) for x in version_info])
