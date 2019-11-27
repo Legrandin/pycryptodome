0adc1903ec518077187b2b8f86c9105324eae440
Helder Eijs <helderijs@gmail.com>
2019-11-27 09:04:33 +0100

    Support for Python 3.8 in metadata

--- setup.py
+++ setup.py
@@ -488,6 +488,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     license="BSD, Public Domain, Apache",
     packages=packages,
