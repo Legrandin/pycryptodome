12692d017938370bb8c61fde8539c19cecb7d2c3
Berkan Teber <berkan@berkanteber.com>
2022-06-29 00:02:39 +0300

    Fix `verify` return types of `PKCS1_PSS` & `PKCS1_v1_5`

--- lib/Crypto/Signature/PKCS1_PSS.pyi
+++ lib/Crypto/Signature/PKCS1_PSS.pyi
@@ -1,7 +1,11 @@
 from typing import Optional, Callable
 
 from Crypto.PublicKey.RSA import RsaKey
-from Crypto.Signature.pss import PSS_SigScheme
+from Crypto.Signature.pss import Hash, _PSS_SigScheme
+
+
+class PSS_SigScheme(_PSS_SigScheme):
+    def verify(self, msg_hash: Hash, signature: bytes) -> bool: ...
 
 
 def new(rsa_key: RsaKey, mgfunc: Optional[Callable]=None, saltLen: Optional[int]=None, randfunc: Optional[Callable]=None) -> PSS_SigScheme: ...
--- lib/Crypto/Signature/PKCS1_v1_5.pyi
+++ lib/Crypto/Signature/PKCS1_v1_5.pyi
@@ -1,6 +1,10 @@
 from Crypto.PublicKey.RSA import RsaKey
 
-from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
+from Crypto.Signature.pkcs1_15 import Hash, _PKCS115_SigScheme
+
+
+class PKCS115_SigScheme(_PKCS115_SigScheme):
+    def verify(self, msg_hash: Hash, signature: bytes) -> bool: ...
 
 
 def new(rsa_key: RsaKey) -> PKCS115_SigScheme: ...
