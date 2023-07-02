d3e871afef5b58064b3d95f533bf6fdc1ef419d3
Helder Eijs <helderijs@gmail.com>
2023-07-02 19:40:48 +0200

    Bump version

--- lib/Crypto/__init__.py
+++ lib/Crypto/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['Cipher', 'Hash', 'Protocol', 'PublicKey', 'Util', 'Signature',
            'IO', 'Math']
 
-version_info = (3, 18, '0')
+version_info = (3, 19, '0b1')
 
 __version__ = ".".join([str(x) for x in version_info])
