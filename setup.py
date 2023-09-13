8bdf6354956281b99b49ff27de3ca39bdbddc56b
Helder Eijs <helderijs@gmail.com>
2023-09-13 14:43:40 +0200

    Add Python 3.12 as supported version

--- setup.py
+++ setup.py
@@ -526,6 +526,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     license="BSD, Public Domain",
     packages=packages,
